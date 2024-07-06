9d523c2b3f7a41dc18354e6283d7e9a869c71c80
yeachan <dus001228@naver.com>
2024-07-07 01:16:36 +0900

    :sparkles: 신규자 db 저장 기능 추가

--- honor_of_war/honor_of_war_new.py
+++ honor_of_war/honor_of_war_new.py
@@ -1,7 +1,8 @@
 from data_control import *
-from PyQt5.QtWidgets import QTableWidget
+from PyQt5.QtWidgets import QTableWidget, QMessageBox
+import datetime
 
-def show_new(screen, table_name):
+def show_new(screen):
     screen.reset_button_styles()
     screen.new_button.setStyleSheet('background-color: lightblue')
     screen.label.setText('참전 명예 수당 지급 신규자')
@@ -10,14 +11,31 @@ def show_new(screen, table_name):
         'Index', '동', '입력날짜', '보훈번호', '성명', '주민번호', '주소',
         '입금유형', '은행명', '예금주', '계좌번호', '신규 사유', '전입일', '비고'
     ])
-    rows = get_data(table_name)
+    rows = get_data("Veterans_New")
+    screen.load_data(rows, 'new')
 
-    screen.table.setRowCount(len(rows))
-    # 폼 필드 전환
-    screen.switch_form_fields('new')
-    for row_idx, row_data in enumerate(rows):
-        for col_idx, col_data in enumerate(row_data):
-            screen.table.setItem(row_idx, col_idx, QTableWidgetItem(str(col_data)))
-    screen.table.resizeColumnsToContents()  # 자동으로 모든 열의 너비를 조정하여 내용을 맞춤
-    screen.table.setMinimumWidth(1000) 
-    screen.table.setSizeAdjustPolicy(QTableWidget.AdjustToContents) 
+def submit_or_edit_form_new(screen):
+    if screen.submit_button.text() == '추가':
+        submit_form(screen)
+    # else:
+    #     screen.update_form()
+
+def submit_form(screen):
+
+    add_data('Veterans_New', (
+            screen.dong_name.text(),
+            datetime.datetime.now().strftime("%Y.%m.%d"),  # 현재 월을 예로 추가 (혹은 필요 시 변경 가능)
+            screen.honor_number.text(),
+            screen.name.text(),
+            screen.resident_number.text(),
+            f"{screen.zip_code.text()} {screen.address.text()} {screen.detail_address.text()}",
+            screen.deposit_type.currentText(),
+            screen.bank_name.currentText(),
+            screen.depositor_name.text(),
+            screen.account_number.text(),
+            screen.new_reason.text(),
+            screen.transfer_date.text(),
+            screen.notes.toPlainText()
+        ))
+    rows = get_data("Veterans_New")
+    screen.load_data(rows, 'new')
